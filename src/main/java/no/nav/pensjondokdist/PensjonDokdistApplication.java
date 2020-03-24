a7da57c600d8f6ada7e458541741399b520b63c2
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-03-24 11:05:24 +0100

    First commit

---
+++ src/main/java/no/nav/pensjondokdist/PensjonDokdistApplication.java
@@ -0,0 +1,13 @@
+package no.nav.pensjondokdist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensjonDokdistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensjonDokdistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/nav/pensjondokdist/PensjonDokdistApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.pensjondokdist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensjonDokdistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
