552f0061443edf7a48ff76420c1a5dc7c77e10ac
Håkon Heggholmen <hakon.heggholmen@nav.no>
2022-06-15 09:30:36 +0200

    Logger request midlertidig.

--- src/main/java/no/nav/pensjondokdist/distribuerjournalpost/DistribuerJournalpostClient.java
+++ src/main/java/no/nav/pensjondokdist/distribuerjournalpost/DistribuerJournalpostClient.java
@@ -1,16 +1,16 @@
 package no.nav.pensjondokdist.distribuerjournalpost;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.nav.pensjondokdist.PensjonDokdistException;
+import no.nav.pensjondokdist.distribuerjournalpost.dto.DistribuerJournalpostRequest;
+import no.nav.pensjondokdist.distribuerjournalpost.dto.DistribuerJournalpostResponse;
+import no.nav.pensjondokdist.oidc.OidcProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import no.nav.pensjondokdist.PensjonDokdistException;
-import no.nav.pensjondokdist.distribuerjournalpost.dto.DistribuerJournalpostRequest;
-import no.nav.pensjondokdist.distribuerjournalpost.dto.DistribuerJournalpostResponse;
-import no.nav.pensjondokdist.oidc.OidcProvider;
 
 @Component
 public class DistribuerJournalpostClient {
@@ -28,6 +28,7 @@ public class DistribuerJournalpostClient {
 
     DistribuerJournalpostResponse post(DistribuerJournalpostRequest request) {
         try {
+            logger.info("url: " + url + "request: " + new ObjectMapper().writeValueAsString(request)); //TODO remove.
             return restTemplate.postForObject(url, new HttpEntity<>(request, oidcProvider.getHeaders()), DistribuerJournalpostResponse.class);
         } catch (Exception e) {
             logger.error("Kunne ikke distribuere journalpost for journalpostId: " + request.getJournalpostId());
