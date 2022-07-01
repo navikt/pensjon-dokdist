c2ac19510dcf8e9d0cd940a00b15df10859713cb
Håkon Heggholmen <hakon.heggholmen@nav.no>
2022-07-01 09:31:10 +0200

    Mock back end skal returnere kun 200 ok, ikke body ved /api/journalpost/:id/send

--- mock-backend.js
+++ mock-backend.js
@@ -6,7 +6,7 @@ app.get('/api/internal/isAlive', (req, res) => res.send('isAlive'));
 app.get('/api/internal/isReady', (req, res) => res.send('isReady'));
 app.post('/api/journalpost/:id/send', (req, res) => setTimeout(() => {
     if (Math.random() < 0.5) {
-        res.status(200).json({ status: 'OK '});
+        res.sendStatus(200);
     } else {
         res.status(500).json({ message: 'This error was intentionally triggered by the mock backend, to test failure situations.' });
     }
