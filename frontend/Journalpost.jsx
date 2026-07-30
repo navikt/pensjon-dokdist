79de7d7b6a1f3bc9944220f951f413c492506d6a
Mads Opheim <mads.opheim@nav.no>
2026-07-30 10:23:45 +0200

    Fjernar react-router-dom, ikkje nødvendig lenger

--- frontend/Journalpost.jsx
+++ frontend/Journalpost.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {useLocation, useParams} from "react-router-dom";
+import {useLocation, useParams} from "react-router";
 import {
     Alert,
     BodyLong,
--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import {StrictMode} from 'react';
 import 'core-js';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router";
 import Journalpost from "./Journalpost";
 
 const rootElement = document.getElementById("root");
